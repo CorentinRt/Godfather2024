224d653d722d055d41b849a25af0b3b38abd0745
Crunchymiam <103903093+Crunchymiam@users.noreply.github.com>
2024-09-11 12:30:47 +0200

    [ADD] phase manager now disables inputs

--- Assets/PhaseManager.cs
+++ Assets/PhaseManager.cs
@@ -3,13 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PhaseManager : MonoBehaviour
 {
 
     [SerializeField]
-    private float _cooldownDuration; 
+    private float _cooldownDuration;
+    [SerializeField]
     private float _VictoryScreenDuration;
+
+    [SerializeField] private PlayerInput _playerInput;
+
+    public PlayerHunterBehavior IronBehavior;
+    public HunterLineBehavior LineBehavior;
+
+    public GameObject Hunter;
+    public GameObject Prey;
+
+    [SerializeField]
+    private float _spawnPoint;
+
+
+    private float _currentTime;
+    private float _endOfCooldown;
     
     void Start()
     {
@@ -19,20 +36,33 @@ public class PhaseManager : MonoBehaviour
     void RoundStart()
     {
         // disable inputs
-        // teleports Players
+        IronBehavior.enabled = false;
+        LineBehavior.enabled = false;
+        _playerInput.actions.FindAction("MouseRotationHunter").Disable();
+        // teleport Players
+        Hunter.transform.position = new Vector3(-_spawnPoint, 0, 0);
+        Prey.transform.position = new Vector3(_spawnPoint, 0, 0);
+
         // countdown animation
+
+
         Invoke("endOfCooldown", _cooldownDuration);
     }
 
     void endOfCooldown()
     {
         // enable inputs
+        _playerInput.actions.FindAction("MouseRotationHunter").Enable();
+        IronBehavior.enabled = true;
+        LineBehavior.enabled = true;
         // start timer
     }
 
 
     void Update()
     {
+        
+
         //  if (pyjama dies || timer ends)
         //  {
         //      if (pyjama dies)
