5c56039e996ff1e51e47356b58b13d423aa1b63e
Corentinrmt <corentin.remot@gmail.com>
2024-09-11 15:00:15 +0200

    [ADD] shake camera + collision wall bounce

--- Assets/Content/Scripts/PlayerHunter/PlayerHunterBehavior.cs
+++ Assets/Content/Scripts/PlayerHunter/PlayerHunterBehavior.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,13 +11,30 @@ public class PlayerHunterBehavior : MonoBehaviour
 
     [SerializeField] private InputActionReference _mouseRotationHunter;
 
-    [SerializeField] private float _speed;
+    [SerializeField] private float _maxSpeed;
+
+    private float _speed;
+    private float _speedLerpPerc;
+    [SerializeField] private float _startAccelerationSpeed;
 
     [SerializeField] private float _rotationSpeed;
 
     [SerializeField] private float _rotationDeceleration;
 
+    [SerializeField] private float _bounceForce;
+    private bool _isBounced;
+    private Coroutine _bounceCooldownCoroutine;
+    [SerializeField] private float _bounceTime;
+
     private Rigidbody2D _rb2D;
+
+    private Coroutine _accelerateToMaxSpeedCoroutine;
+
+    private Tweener _cameraShakeTween;
+
+    [SerializeField] private float _cameraShakeTime;
+    [SerializeField] private float _cameraShakeForce;
+
 
 
     private void Awake()
@@ -26,6 +44,8 @@ public class PlayerHunterBehavior : MonoBehaviour
     private void Start()
     {
         _mouseRotationHunter.action.performed += UpdateRotation;
+
+        StartAccelerateToMaxSpeed();
     }
     private void OnDestroy()
     {
@@ -38,6 +58,9 @@ public class PlayerHunterBehavior : MonoBehaviour
         HunterMove();
     }
 
+    #region Move
+
+
     private void UpdateRotation(InputAction.CallbackContext context)
     {
         Vector2 tempVect = context.ReadValue<Vector2>();
@@ -47,9 +70,11 @@ public class PlayerHunterBehavior : MonoBehaviour
 
     private void HunterMove()
     {
-        _rb2D.velocity = transform.up * Time.deltaTime * _speed;
+        if (!_isBounced)
+        {
+            _rb2D.velocity = transform.up * Time.deltaTime * _speed;
+        }
     }
-
     private void DecelerateRotation()
     {
         if (Mathf.Abs(_rb2D.angularVelocity) > 1f)
@@ -60,5 +85,109 @@ public class PlayerHunterBehavior : MonoBehaviour
         {
             _rb2D.angularVelocity = 0f;
         }
+    }
+    #endregion
+
+    private void Bounce(Vector2 dir)
+    {
+        if (_rb2D == null)
+            return;
+
+        if (_cameraShakeTween != null)
+        {
+            _cameraShakeTween.Complete();
+        }
+
+        _cameraShakeTween = Camera.main.DOShakePosition(_cameraShakeTime, _cameraShakeForce);
+
+        _isBounced = true;
+        StartBounceCooldown();
+        _rb2D.AddForce(dir * _bounceForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.gameObject.CompareTag("Wall"))
+            {
+                if (collision.contactCount == 0)
+                    return;
+
+                Vector2 tempVect = new Vector2(transform.position.x, transform.position.y) - collision.contacts[0].point;
+
+                tempVect.Normalize();
+
+                StartAccelerateToMaxSpeed();
+
+                Bounce(tempVect);
+            }
+        }
+    }
+    private void StartBounceCooldown()
+    {
+        StopBounceCooldown();
+
+        _bounceCooldownCoroutine = StartCoroutine(BounceCooldownCoroutine());
+    }
+    private void StopBounceCooldown()
+    {
+        if (_bounceCooldownCoroutine != null)
+        {
+            StopCoroutine(_bounceCooldownCoroutine);
+
+            _bounceCooldownCoroutine = null;
+        }
+    }
+    private IEnumerator BounceCooldownCoroutine()
+    {
+        float timer = _bounceTime;
+
+        while (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+
+            yield return null;
+        }
+
+        _isBounced = false;
+
+        StopBounceCooldown();
+
+        yield return null;
+    }
+
+
+    [Button] private void StartAccelerateToMaxSpeed()
+    {
+        StopAccelerationToMaxSpeed();
+
+        _speedLerpPerc = 0;
+
+        _speed = 0;
+
+        _accelerateToMaxSpeedCoroutine = StartCoroutine(AccelerateToMaxSpeedCoroutine());
+    }
+    private void StopAccelerationToMaxSpeed()
+    {
+        if (_accelerateToMaxSpeedCoroutine != null)
+        {
+            StopCoroutine(_accelerateToMaxSpeedCoroutine);
+
+            _accelerateToMaxSpeedCoroutine = null;
+        }
+    }
+    private IEnumerator AccelerateToMaxSpeedCoroutine()
+    {
+        while (_speedLerpPerc <= 1f)
+        {
+            _speed = Mathf.Lerp(_speed, _maxSpeed, _speedLerpPerc);
+
+            _speedLerpPerc += Time.deltaTime * _startAccelerationSpeed;
+
+            yield return null;
+        }
+
+        yield return null;
     }
 }
