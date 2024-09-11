858accf32b94c5955854f07bc2cfbf1fa3f8072d
Corentinrmt <corentin.remot@gmail.com>
2024-09-11 15:09:41 +0200

    [ADD] slide effect part 1

---
+++ Assets/Content/Scripts/UIEffects/SlideToEffect.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideToEffect : MonoBehaviour
+{
+    [SerializeField] private Transform _start;
+    [SerializeField] private Transform _end;
+}
